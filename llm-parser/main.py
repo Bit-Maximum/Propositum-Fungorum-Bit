3b71f4face9162f32d57bfc80876fcd457884579
XoTTaB <jassik2002@mail.ru>
2026-02-05 19:48:00 +1000

    FEAT: Add routes

--- llm-parser/main.py
+++ llm-parser/main.py
@@ -1,8 +1,13 @@
+import json
+from io import BytesIO
 from pathlib import Path
-from fastapi import FastAPI, UploadFile, APIRouter
+
+import requests
+from fastapi import FastAPI, UploadFile, APIRouter, Body, HTTPException
 import uvicorn
 from app.api.parse import PDFParser
 from app.pipeline.pipeline import Pipeline
+from app.config import settings
 
 app = FastAPI(
     title="LLM Parser API",
@@ -23,8 +28,52 @@ router: APIRouter = APIRouter(prefix='/api/parser')
 
 file_text: str  # Пока так потом в сессию закинуть можно
 
+
+def upload_json_as_file(data, filename="data.json", base_url="http://localhost:8000"):
+    """
+    Формирует файл из переменной с данными и отправляет на эндпоинт
+
+    Args:
+        data (dict | list | str): Данные для отправки (dict/list или уже сериализованный JSON)
+        filename (str): Имя файла для отправки
+        base_url (str): Базовый URL сервиса
+
+    Returns:
+        dict: Ответ сервера в формате JSON
+    """
+    url = f"{base_url}{settings.BACKEND_UPLOAD_PATH}"
+
+    # 1. Преобразуем данные в JSON-строку (если это ещё не строка)
+    if isinstance(data, (dict, list)):
+        json_str = json.dumps(data, ensure_ascii=False)
+    else:
+        json_str = str(data)
+
+    # 2. Создаём "файл" в памяти из строки
+    file_content = json_str.encode('utf-8')
+    file_obj = BytesIO(file_content)
+
+    # 3. Отправляем как файл
+    files = {
+        'file': (filename, file_obj, 'application/json')
+    }
+
+    response = requests.post(
+        url,
+        files=files,
+        timeout=1200  # 20 минут
+    )
+
+    response.raise_for_status()
+    return response.json()
+
 @router.post("/")
-async def parse_pdf(file: UploadFile):
+async def parse_pdf(
+        file: UploadFile,
+        display_name: str,
+        subtitle_name: str,
+):
+
     tmp_path = Path("/tmp") / file.filename
     tmp_path.write_bytes(file.file.read())
 
@@ -32,7 +81,33 @@ async def parse_pdf(file: UploadFile):
     global file_text
     file_text = parser.parse(str(tmp_path))
 
-    return await pipeline.run(file_text)
+    graph_json = await pipeline.run(file_text)
+
+    file_path = upload_json_as_file(
+        data=graph_json,
+        base_url=settings.BACKEND_HOST,
+    )
+
+    try:
+        s3_path = file_path["s3_path"]
+    except KeyError as e:
+        raise HTTPException(
+            status_code=400,
+        )
+    print(s3_path)
+    response = requests.post(
+        url=f"{settings.BACKEND_HOST}{settings.BACKEND_METADATA_PATH}",
+        json={
+            "display_name": display_name,
+            "subtitle_name": subtitle_name,
+            "questionnaire_path": s3_path,
+        }
+    )
+    print(response.json())
+
+
+
+    return graph_json
 
 @router.get("/healthcheck")
 async def healthcheck():
