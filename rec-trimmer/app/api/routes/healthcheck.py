f36d2d9d0a2decea819797208bb6ff3e24f10bea
Artem Shuliatev <slenderartem2003@gmail.com>
2026-03-18 22:05:34 +1000

    FEAT(routes): Добавлен эндпоинт для healthcheck

---
+++ rec-trimmer/app/api/routes/healthcheck.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter, status
+
+
+router: APIRouter = APIRouter(
+    prefix="/healthcheck",
+    tags=["healthcheck",]
+)
+
+
+@router.get(path="/", status_code=status.HTTP_200_OK,)
+async def get_healthcheck():
+    return {
+        "status": "ok"
+    }
