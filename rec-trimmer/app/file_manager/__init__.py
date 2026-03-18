18015f9509553d27c7823584059e8a151608d83b
Artem Shuliatev <slenderartem2003@gmail.com>
2026-03-18 22:04:36 +1000

    FEAT(file_manager): Добавлены импорты в __init__.py

---
+++ rec-trimmer/app/file_manager/__init__.py
@@ -0,0 +1,23 @@
+from app.file_manager import (
+    pipeline,
+    file_analyzer,
+    file_parser,
+    id_generator,
+    merge_tool,
+    models,
+    file_repository,
+    page_range_resolver,
+    similarity_calculator,
+)
+
+__all__ = [
+    'pipeline',
+    'file_analyzer',
+    'file_parser',
+    'id_generator',
+    'merge_tool',
+    'models',
+    'file_repository',
+    'page_range_resolver',
+    'similarity_calculator',
+]
