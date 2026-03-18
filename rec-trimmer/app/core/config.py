0d0b23c4cea53b7b3ff07618896a6143d247db52
Artem Shuliatev <slenderartem2003@gmail.com>
2026-03-18 21:51:36 +1000

    FEAT: Добавлена конфигурация приложения

--- rec-trimmer/app/core/config.py
+++ rec-trimmer/app/core/config.py
@@ -1,5 +1,26 @@
+from pathlib import Path
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+
+
+class StorageSettings(BaseModel):
+    pass
+
+
+class LocalStorageSettings(StorageSettings):
+    ALLOWED_EXTENSIONS: set[str] = Field(
+        default_factory=lambda: {'pdf'},
+        description="Разрешенные расширения файлы"
+    )
+    ROOT: Path = Field(
+        default_factory=lambda: Path(__file__).resolve().parent.parent.parent,
+        description="Корневая директория проекта"
+    )
+
+    @property
+    def LOCAL_UPLOAD_DIR(self) -> Path:
+        return self.ROOT / "uploads"
 
 
 class AppSettings(BaseModel):
@@ -8,11 +29,13 @@ class AppSettings(BaseModel):
 
 
 class Settings(BaseSettings):
-    APP: AppSettings
+    STORAGE: LocalStorageSettings = Field(default_factory=LocalStorageSettings)
+    APP: AppSettings = Field(default_factory=AppSettings)
 
     model_config = SettingsConfigDict(
         env_file='.env',
         env_nested_delimiter='__',
+        extra='ignore',
         env_file_encoding='utf-8',
     )
 
