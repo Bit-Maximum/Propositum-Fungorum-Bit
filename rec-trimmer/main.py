3bda3486860a81730a58534741dd264959e0c39f
Artem Shuliatev <slenderartem2003@gmail.com>
2026-03-18 22:09:55 +1000

    FEAT(main): Добавлена возможность запуска приложения через main.py

--- rec-trimmer/main.py
+++ rec-trimmer/main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from rec-trimmer!")
+from typing import TYPE_CHECKING
+import logging
+
+import uvicorn
+
+from app import create_app, settings
+
+if TYPE_CHECKING:
+    from fastapi import FastAPI
+
+
+logger = logging.getLogger(__name__)
 
 
 if __name__ == "__main__":
-    main()
+    app: 'FastAPI' = create_app()
+
+    logger.warning("Запуск приложения...")
+
+    uvicorn.run(
+        app=app,
+        host=settings.APP.HOST,
+        port=settings.APP.PORT,
+    )
